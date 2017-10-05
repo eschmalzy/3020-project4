684831f7d6640525db1b05a33be2381da6fb1884
Ethan Schmalz <ethan@Ethans-Mac.local>
2017-10-05 15:21:39 -0600

    Done with chapter 16

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -12,7 +12,7 @@ class ItemsViewController: UITableViewController {
 
     var itemStore: ItemStore!
     
-    @IBAction func addNewItem(_ sender: UIButton){
+    @IBAction func addNewItem(_ sender: UIBarButtonItem){
         let newItem = itemStore.createItem()
         
         if let index = itemStore.allItems.index(of: newItem) {
@@ -20,25 +20,16 @@ class ItemsViewController: UITableViewController {
             tableView.insertRows(at: [indexPath], with: .automatic)
         }
     }
-    
-    
-    @IBAction func toggleEditingMode(_ sender: UIButton){
-        if isEditing{
-            sender.setTitle("Edit", for: .normal)
-            setEditing(false, animated: true)
-        } else{
-            sender.setTitle("Done", for: .normal)
-            setEditing(true, animated: true)
-        }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-        tableView.contentInset = insets
-        tableView.scrollIndicatorInsets = insets
+        navigationItem.leftBarButtonItem = editButtonItem
     
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 65
@@ -93,6 +84,19 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier{
+        case "showItem"?:
+            if let row = tableView.indexPathForSelectedRow?.row{
+                let item = itemStore.allItems[row]
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.item = item
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier.")
+        }
     }
     
 }
--- Homepwner/item.swift
+++ Homepwner/item.swift
@@ -9,12 +9,31 @@
 import Foundation
 
 
-class Item: NSObject {
+class Item: NSObject, NSCoding {
     
     var name: String
     var valueInDollars: Int
     var serialNumber: String?
     var dateCreated: Date
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(dateCreated, forKey:"dateCreated")
+//        aCoder.encode(itemKey, forKey: "itemKey")
+        aCoder.encode(serialNumber, forKey: "serialNumber")
+        
+        aCoder.encode(valueInDollars, forKey: "valueInDollars")
+    }
+    
+    required init(coder aDecoder: NSCoder){
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        dateCreated = aDecoder.decodeObject(forKey: "dateCreated") as! Date
+        serialNumber = aDecoder.decodeObject(forKey: "serialNumber") as! String?
+        valueInDollars = aDecoder.decodeInteger(forKey: "valueInDollars")
+        
+        super.init()
+    }
+    
     
     //designated initializer
     init(name: String, serialNumber: String?, valueInDollars: Int){
